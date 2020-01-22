ca1ca3440ea2a11ae38ab53e11f8b96e957405d8
unknown <you@example.com>
2020-01-22 17:18:52 +0300

    add new pages for mobile dising

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+let productBtn = document.querySelectorAll(".catalog__btn");
+productBtn.forEach(item => {
+  item.addEventListener("click", addOrder);
+});
+function addOrder() {
+  event.preventDefault();
+  let infoOrder = document.querySelector(".info__order");
+  let busketValue = document.querySelector(".busket__value");
+  infoOrder.classList.add("is-active");
+  busketValue.classList.add("is-active");
+  this.classList.toggle("is-active");
+}
+
+// let btnAdd = document.querySelector(".btn");
+// let btnRemoveActive = document.querySelector(".product__btn__less");
+
+// btnAdd.addEventListener("click", addOrderTwo);
+
+// function addOrderTwo() {
+//   event.preventDefault();
+//   let infoOrder = document.querySelector(".info__order");
+//   infoOrder.classList.add("is-active");
+//   btnRemoveActive.classList.add("is-active");
+// }
+
+let myNav = function() {
+  let burgerBtn = document.querySelector(".burger__menu__button");
+  burgerBtn.addEventListener("click", activeBtnToggle);
+  function activeBtnToggle() {
+    let burgerMenuBtn = document.querySelector(".burger__menu");
+    let leftMenu = document.querySelector(".header__nav");
+    burgerMenuBtn.classList.toggle("is-active");
+    leftMenu.classList.toggle("is-active");
+  }
+};
+myNav();
