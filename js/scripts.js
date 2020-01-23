7ed6a1ae3c81c777fbf87281a933b4c6888374ed
unknown <you@example.com>
2020-01-23 16:45:16 +0300

    add user area pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-let productBtn = document.querySelectorAll(".catalog__btn");
-productBtn.forEach(item => {
-  item.addEventListener("click", addOrder);
-});
-function addOrder() {
-  event.preventDefault();
-  let infoOrder = document.querySelector(".info__order");
-  let busketValue = document.querySelector(".busket__value");
-  infoOrder.classList.add("is-active");
-  busketValue.classList.add("is-active");
-  this.classList.toggle("is-active");
-}
+// let productBtn = document.querySelectorAll(".catalog__btn");
+// productBtn.forEach(item => {
+//   item.addEventListener("click", addOrder);
+// });
+// function addOrder() {
+//   event.preventDefault();
+//   let infoOrder = document.querySelector(".info__order");
+//   let busketValue = document.querySelector(".busket__value");
+//   infoOrder.classList.add("is-active");
+//   busketValue.classList.add("is-active");
+//   this.classList.toggle("is-active");
+// }
 
 // let btnAdd = document.querySelector(".btn");
 // let btnRemoveActive = document.querySelector(".product__btn__less");
@@ -34,3 +34,11 @@ let myNav = function() {
   }
 };
 myNav();
+
+let personalAreaBtn = document.querySelectorAll(".history__list button");
+personalAreaBtn.forEach(item => {
+  item.addEventListener("click", showOrders);
+});
+function showOrders() {
+  this.classList.add("is-active");
+}
