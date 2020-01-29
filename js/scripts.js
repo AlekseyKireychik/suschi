762d93ab7d707d330a361022230a951f5c9e98a7
AlekseyKireychik <kireychika@mail.ru>
2020-01-30 00:52:54 +0300

    fix size input area in delivery. add styles promocode batton. add info block for check deliver pay

--- js/scripts.js
+++ js/scripts.js
@@ -11,22 +11,28 @@ function addOrder() {
   this.classList.toggle("is-active");
 }
 
-let btnAdd = document.querySelectorAll(".btn");
+let btnAdd = document.querySelectorAll(".product__btn__add");
 let btnRemoveActive = document.querySelector(".product__btn__less");
+
 btnAdd.forEach(item => {
   item.addEventListener("click", addOrderTwo);
 });
 
 function addOrderTwo() {
+
   event.preventDefault();
   let infoOrder = document.querySelector(".info__order");
   infoOrder.classList.add("is-active");
   btnRemoveActive.classList.add("is-active");
+
 }
 
+
 let myNav = function() {
-  let burgerBtn = document.querySelector(".burger__menu__button");
-  burgerBtn.addEventListener("click", activeBtnToggle);
+  let burgerBtn = document.querySelectorAll(".burger__menu__button");
+  burgerBtn.forEach(item => {
+    item.addEventListener("click", activeBtnToggle);
+  });
   function activeBtnToggle() {
     let burgerMenuBtn = document.querySelector(".burger__menu");
     let leftMenu = document.querySelector(".header__nav");
@@ -40,6 +46,7 @@ let personalAreaBtn = document.querySelectorAll(".history__list button");
 personalAreaBtn.forEach(item => {
   item.addEventListener("click", showOrders);
 });
+
 function showOrders() {
   this.classList.add("is-active");
   this.innerHTML = "Оставить отзыв";
